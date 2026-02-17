a8a9990c577f6aaabf33a1d9d5ddfb08ef32ce38
penart <vkuzugashev@mail.ru>
2026-02-17 12:24:45 +0700

    fix create table with timezone

--- historian/store/init.sql
+++ historian/store/init.sql
@@ -5,7 +5,7 @@
 CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
 
 CREATE TABLE history_bools (
-        tag_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+        tag_time TIMESTAMP WITH TIME ZONE NOT NULL,
         tag_id VARCHAR(50) NOT NULL,
         status INTEGER NOT NULL,
         value BOOLEAN,
@@ -13,7 +13,7 @@ CREATE TABLE history_bools (
 );
 
 CREATE TABLE history_integers (
-        tag_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+        tag_time TIMESTAMP WITH TIME ZONE NOT NULL,
         tag_id VARCHAR(50) NOT NULL,
         status INTEGER NOT NULL,
         value INTEGER,
@@ -22,7 +22,7 @@ CREATE TABLE history_integers (
 
 
 CREATE TABLE history_floats (
-        tag_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+        tag_time TIMESTAMP WITH TIME ZONE NOT NULL,
         tag_id VARCHAR(50) NOT NULL,
         status INTEGER NOT NULL,
         value FLOAT,
@@ -31,7 +31,7 @@ CREATE TABLE history_floats (
 
 
 CREATE TABLE history_strings (
-        tag_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+        tag_time TIMESTAMP WITH TIME ZONE NOT NULL,
         tag_id VARCHAR(50) NOT NULL,
         tag_type VARCHAR(10) NOT NULL,
         status INTEGER NOT NULL,
